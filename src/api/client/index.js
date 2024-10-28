82d6552ee5666896e3d11d879d3b3070c57579d2
mswarnab <mswarnab008@gmail.com>
2024-10-28 21:22:28 +0530

    final part 1 v01

--- src/api/client/index.js
+++ src/api/client/index.js
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
 export const client = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://popo-backend-1.onrender.com/',
   timeout: 10000000000,
   withCredentials: true
-  // headers: {
-  //   'x-auth-token':
-  //     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6IlRJVElSUEVUU0hPUEBQME85SThVNyIsImlhdCI6MTcyODg3OTczMX0.K14-dWjVmj-vbylSvFulQWioZr6flpTMz0sutuQ_FbM'
-  // }
 });
