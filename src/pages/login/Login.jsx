1299ea57b798417ae015708ea01bed7ad2b683dc
Supratim-S <supratimsarkar007@outlook.com>
2025-01-17 01:13:05 +0530

    router try fixed take 2

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -35,7 +35,7 @@ function Login() {
   useEffect(() => {
     (async () => {
       await client.get('/auth');
-      console.log('hello here');
+      console.log(window.history.previous.href);
       navigate('/');
       return () => {
         return null;
