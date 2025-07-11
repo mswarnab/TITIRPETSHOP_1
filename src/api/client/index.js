cc1681dce6b35a32710c93edfecfd249023d5880
mswarnab <mswarnab008@gmail.com>
2025-07-12 04:39:52 +0530

    Swarnab backend vercel fix v2

--- src/api/client/index.js
+++ src/api/client/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const client = axios.create({
-  // baseURL: 'https://popo-backend-1.onrender.com',
-  baseURL: 'https://popo-backend.vercel.app/',
+  baseURL: 'https://popo-backend-1.onrender.com',
+  // baseURL: 'https://popo-backend.vercel.app/',
   // baseURL: 'http://localhost:8000',
 
   // baseURL: 'https://popo-backend-dev.onrender.com',
